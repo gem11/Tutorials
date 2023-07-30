6183819a18161994343d44382adcff4f1dfc95e7
Martin Gernss <martin@gernss.com>
2023-07-30 12:18:12 +0200

    Updated classes project

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -2,60 +2,58 @@
 
 namespace Classes
 {
+    class FirstClass
+    {
+        string text;
+
+        public FirstClass(string text)
+        {
+            this.text = text;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine(text);
+        }
+    }
+
     class MainClass
     {
         public static void Main(string[] args)
         {
+            // new creates a new object of type 'FirstClass'
+            FirstClass firstClass = new FirstClass("Hello World");
+            firstClass.SayHello();
+            Console.WriteLine();
+
+            // The following code uses a class defined in SimpleClass.cs:
+
+            // New instance of SimpleClass, passing '1' to constructor:
             SimpleClass sc1 = new SimpleClass(1);
             sc1.WriteNumber();
             Console.WriteLine();
 
+            // We let the class incrase the number and check the result:
             sc1.IncreaseNumber();
             sc1.WriteNumber();
             Console.WriteLine();
 
+            // We declare 'alsoSc1', which links to the same object:
             SimpleClass alsoSc1 = sc1;
-            alsoSc1.IncreaseNumber();
-            sc1.WriteNumber();
+            alsoSc1.IncreaseNumber(); // Increasing number on alsoSc1
+            sc1.WriteNumber(); // sc1 is affected
             Console.WriteLine();
 
-            sc1 = null;
-            alsoSc1.WriteNumber();
+            sc1 = null; // sc1 is set to null => not pointing to any object
+            alsoSc1.WriteNumber(); // alsoSc1 still points to the object
             Console.WriteLine();
 
-            // Private members not accessible:
+            // Private members are not accessible and will create compiler error
+            // Try to uncomment the following lines:
             // sc1.number = 5;
             // sc1.PerformIncrease();
         }
     }
 
-    class SimpleClass
-    {
-        private int number; // Private Member variable
-        
-        public SimpleClass(int number) // Constructor
-        {
-            this.number = number;
-        }
-
-        public void WriteNumber() // Public Instance Method
-        {
-            Console.WriteLine("number = " + number);
-        }
-
-        private void PerformIncrease() // Private Instance Method
-        {
-            this.number = this.number + 1;
-        }
-
-        public void IncreaseNumber() // Public Instance Method
-        {
-            PerformIncrease();
-        }
-
-        public static void WriteSomething() // Static Method
-        {
-            Console.WriteLine("This is a static method");
-        }
-    }
+    
 }
---
+++ Classes/SimpleClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Classes
+{
+    class SimpleClass
+    {
+        private int number; // Private Member variable
+
+        public int Number // Public readonly Property
+        {
+            get { return number; }
+        }
+
+        public SimpleClass(int number) // Constructor
+        {
+            this.number = number;
+        }
+
+        public void WriteNumber() // Public Instance Method
+        {
+            Console.WriteLine("number = " + number);
+        }
+
+        private void PerformIncrease() // Private Instance Method
+        {
+            this.number = this.number + 1;
+        }
+
+        public void IncreaseNumber() // Public Instance Method
+        {
+            PerformIncrease();
+        }
+
+        public static void WriteSomething() // Static Method
+        {
+            Console.WriteLine("This is a static method");
+        }
+    }
+}
